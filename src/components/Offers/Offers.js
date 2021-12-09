18f08a5ab08287cd2b79a55b97565003dd7fbf7c
MrHamdan <mr.hamdanahmed@gmail.com>
2021-12-09 20:57:15 +0600

    offer section added

--- src/components/Offers/Offers.js
+++ src/components/Offers/Offers.js
@@ -9,43 +9,39 @@ const Offers = () => {
     return (
         <div className="offer-section">
             <div className="offers-bg-color d-flex justify-content-evenly">
-                <div class="card" style={{ width: '18rem' }}>
-                    <div class="card-body">
+                <div className="" style={{ width: '18rem' }}>
+                    <div className="card-body">
                         <img src={offer1} alt="" className="offer-image mx-auto" />
-                        <h6 class="card-subtitle mb-2  mt-2 fs-4 fw-bold">SPECIAL OFFERS</h6>
-                        <p class="card-text text-muted">Shop Big Save Big</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <h6 className="card-subtitle mb-2  mt-2 fs-4 fw-bold">SPECIAL OFFERS</h6>
+                        <p className="card-text text-muted">Shop Big Save Big</p>
+
                     </div>
                 </div>
-                <div class="card" style={{ width: '18rem' }}>
-                    <div class="card-body">
+                <div className="" style={{ width: '18rem' }}>
+                    <div className="card-body">
                         <img src={offer2} alt="" className="offer-image mx-auto" />
 
-                        <h6 class="card-subtitle mb-2  mt-2 fs-4 fw-bold">FREE DELIVERY</h6>
-                        <p class="card-text text-muted">On Orders Above $99</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <h6 className="card-subtitle mb-2  mt-2 fs-4 fw-bold">FREE DELIVERY</h6>
+                        <p className="card-text text-muted">On Orders Above $99</p>
+
                     </div>
                 </div>
-                <div class="card" style={{ width: '18rem' }}>
-                    <div class="card-body">
+                <div className="" style={{ width: '18rem' }}>
+                    <div className="card-body">
                         <img src={offer3} alt="" className="offer-image mx-auto" />
 
-                        <h6 class="card-subtitle mb-2  mt-2 fs-4 fw-bold">30 DAYS RETURN</h6>
-                        <p class="card-text text-muted">Policy We Offers</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <h6 className="card-subtitle mb-2  mt-2 fs-4 fw-bold">30 DAYS RETURN</h6>
+                        <p className="card-text text-muted">Policy We Offers</p>
+
                     </div>
                 </div>
-                <div class="card" style={{ width: '18rem' }}>
-                    <div class="card-body">
+                <div className="" style={{ width: '18rem' }}>
+                    <div className="card-body">
                         <img src={offer4} alt="" className="offer-image mx-auto" />
 
-                        <h6 class="card-subtitle mb-2  mt-2 fs-4 fw-bold">FASTEST SHIPPING</h6>
-                        <p class="card-text text-muted">2 Days Express</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <h6 className="card-subtitle mb-2  mt-2 fs-4 fw-bold">FASTEST SHIPPING</h6>
+                        <p className="card-text text-muted">2 Days Express</p>
+
                     </div>
                 </div>
             </div>
